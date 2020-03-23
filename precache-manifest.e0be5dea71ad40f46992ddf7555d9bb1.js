3f29b0999f00402b88bc5b35d8df689df9fdd258
Anuj Gupta <junagupta19@gmail.com>
2020-03-24 00:09:07 +0530

    Updates

---
+++ precache-manifest.e0be5dea71ad40f46992ddf7555d9bb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92808b1f52c126e6055f8f43021dd27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "598c8540064c1a7449a1",
+    "url": "/static/js/2.6d458160.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/static/js/2.6d458160.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16c6d431b939b8bd2d0",
+    "url": "/static/js/main.a476a7eb.chunk.js"
+  },
+  {
+    "revision": "6ac920e522db4d553921",
+    "url": "/static/js/runtime-main.d45b088e.js"
+  },
+  {
+    "revision": "1484f0a25740b536ad5600edeaf56b38",
+    "url": "/static/media/bus.1484f0a2.svg"
+  }
+]);
